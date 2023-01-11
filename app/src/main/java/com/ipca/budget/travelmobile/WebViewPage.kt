4582b93e0ea5b11b8b9a5c684d70c93d0c6006ad
Zeet76 <a23496@alunos.ipca.pt>
2023-01-11 12:51:11 +0000

    Design

--- app/src/main/java/com/ipca/budget/travelmobile/WebViewPage.kt
+++ app/src/main/java/com/ipca/budget/travelmobile/WebViewPage.kt
@@ -1,22 +1,35 @@
 package com.ipca.budget.travelmobile
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class WebViewPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
-        val url = intent.getStringExtra("url")
-
-        url?.let {
-            findViewById<WebView>(R.id.webpageView).loadUrl(it)
+        val url1 = intent.getStringExtra("url")
+        val webView = findViewById<WebView>(R.id.webpageView)
+        url1?.let {
+            webView.loadUrl(it)
         }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
